2037453b08cd121aed9ae76f211a18da8e039c97
MarZorn <Marvin.Zorn@Postbank.de>
2016-11-02 16:11:44 +0100

    Create HelloWorld.java
    Developed a Java program which prints HelloWorld.

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class HelloWorld{
+
+  public static void main(String[] args){
+    JFrame frame = new JFrame();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.add(new JLable("Hello World.");
+  }
+  
+}
